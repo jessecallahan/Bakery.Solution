7833392ad2f890a37c142ee92108d5ab9d2b354b
Jesse Callahan <jessetylercallahan@gmail.com>
2021-04-22 21:32:34 -0700

    initialize git, set up proj

---
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -0,0 +1,22 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+using System.Collections.Generic;
+using System;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class PastryTests
+  {
+
+    [TestMethod]
+    public void ItemConstructor_CreatesInstanceOfItem_Item()
+    {
+      Item newItem = new Item("test");
+      Assert.AreEqual(typeof(Item), newItem.GetType());
+    }
+
+
+
+  }
+}
---
+++ Bakery/Program.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using Bakery.Models;
+
+namespace Bakery
+{
+  public class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("et phone home");
+
+    }
+  }
+}
